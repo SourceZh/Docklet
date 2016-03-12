5f93be9300ee4b51bdc5a795842207907e22ac27
libao14 <libao14@pku.edu.cn>
2016-03-12 16:53:50 +0800

    [nettools] update ipcontrol by ip command of linux

--- src/nettools.py
+++ src/nettools.py
@@ -17,11 +17,12 @@ class ipcontrol(object):
                 thislink = blocks[1].strip(':')
                 links[thislink] = {}
                 links[thislink]['state'] = blocks[blocks.index('state')+1] if 'state' in blocks else 'UNKNOWN'
-                links[thislink]['inet'] = []
             # Level 2 : line with 4 spaces
             elif line[4] != ' ':
                 blocks = line.split()
                 if blocks[0] == 'inet':
+                    if 'inet' not in links[thislink]:
+                        links[thislink]['inet'] = []
                     links[thislink]['inet'].append(blocks[1])
                 # we just need inet (IPv4)
                 else:  
@@ -34,24 +35,78 @@ class ipcontrol(object):
     @staticmethod
     def list_links():
         try:
-            ret = subprocess.run(['ip', 'address'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False, check=True)
-            return iptool.parse(ret.stdout.decode('utf-8'))
+            ret = subprocess.run(['ip', 'link', 'show'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False, check=True)
+            links = ipcontrol.parse(ret.stdout.decode('utf-8'))
+            return [True, list(links.keys())]
         except subprocess.CalledProcessError as suberror:
-            print ("list links failed : %s" % suberror.stdout.decode('utf-8'))
-            return {}
-
+            return [False, "list links failed : %s" % suberror.stdout.decode('utf-8')]
+            
     @staticmethod
-    def get_link(linkname):
+    def link_exist(linkname):
         try:
-            ret = subprocess.run(['ip', 'address', 'show', str(linkname)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False, check=True)
-            return iptool.parse(ret.stdout.decode('utf-8'))[linkname]
-        except subprocess.CalledProcessError as suberror:
-            print ("get link info failed : %s" % suberror.stdout.decode('utf-8'))
+            subprocess.run(['ip', 'link', 'show', 'dev', linkname], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False, check=True)
+            return True
+        except subprocess.CalledProcessError:
             return False
 
     @staticmethod
+    def link_info(linkname):
+        try:
+            ret = subprocess.run(['ip', 'address', 'show', 'dev', str(linkname)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False, check=True)
+            return [True, ipcontrol.parse(ret.stdout.decode('utf-8'))[linkname]]
+        except subprocess.CalledProcessError as suberror:
+            return [False, "get link info failed : %s" % suberror.stdout.decode('utf-8')]
+
+    @staticmethod
+    def link_state(linkname):
+        try:
+            ret = subprocess.run(['ip', 'link', 'show', 'dev', str(linkname)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False, check=True)
+            return [True, ipcontrol.parse(ret.stdout.decode('utf-8'))[linkname]['state']]
+        except subprocess.CalledProcessError as suberror:
+            return [False, "get link state failed : %s" % suberror.stdout.decode('utf-8')]
+
+    @staticmethod
+    def link_ips(linkname):
+        [status, info] = ipcontrol.link_info(linkname)
+        if status:
+            if 'inet' not in info:
+                return [True, []]
+            else:
+                return [True, info['inet']]
+        else:
+            return [False, info]
+
+    @staticmethod
+    def up_link(linkname):
+        try:
+            subprocess.run(['ip', 'link', 'set', 'dev', str(linkname), 'up'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False, check=True)
+            return [True, linkname]
+        except subprocess.CalledProcessError as suberror:
+            return [False, "set link up failed : %s" % suberror.stdout.decode('utf-8')]
+
+    @staticmethod
+    def down_link(linkname):
+        try:
+            subprocess.run(['ip', 'link', 'set', 'dev', str(linkname), 'down'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False, check=True)
+            return [True, linkname]
+        except subprocess.CalledProcessError as suberror:
+            return [False, "set link down failed : %s" % suberror.stdout.decode('utf-8')]
+
+    @staticmethod
     def add_addr(linkname, address):
-        pass
+        try:
+            subprocess.run(['ip', 'address', 'add', address, 'dev', str(linkname)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False, check=True)
+            return [True, linkname]
+        except subprocess.CalledProcessError as suberror:
+            return [False, "add address failed : %s" % suberror.stdout.decode('utf-8')]
+
+    @staticmethod
+    def del_addr(linkname, address):
+        try:
+            subprocess.run(['ip', 'address', 'del', address, 'dev', str(linkname)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False, check=True)
+            return [True, linkname]
+        except subprocess.CalledProcessError as suberror:
+            return [False, "delete address failed : %s" % suberror.stdout.decode('utf-8')]
 
 
 class ovscontrol(object):
